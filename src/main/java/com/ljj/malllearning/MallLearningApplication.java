f0dd77e909fb43a69397b10c57dcf88b88940fd3
liujingjing <782744521@qq.com>
2021-08-17 23:18:24 +0800

    Reactive web容器

--- src/main/java/com/ljj/malllearning/MallLearningApplication.java
+++ src/main/java/com/ljj/malllearning/MallLearningApplication.java
@@ -2,10 +2,13 @@ package com.ljj.malllearning;
 
 import com.ljj.malllearning.util.SpringLifeCycleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.context.WebServerApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 //排除数据库的配置
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
@@ -15,4 +18,10 @@ public class MallLearningApplication {
         SpringApplication.run(MallLearningApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner runner(WebServerApplicationContext context){
+        return args -> {
+            System.out.println("当前server实现类为："+context.getWebServer().getClass().getName());
+        };
+    }
 }
